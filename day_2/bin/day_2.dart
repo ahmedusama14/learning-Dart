15a81fc3702534a7328d0dc15179a443a6193439
ahmedusama14 <ahmedbnsaid5555@gmail.com>
2025-12-27 00:06:34 +0200

    my Day 2 in Dart!!

---
+++ day_2/bin/day_2.dart
@@ -0,0 +1,12 @@
+void main() 
+{
+  num age = 25;
+  String fName = "Ahmed";
+  String lName = "Usama";
+  int phone = 01015376057;
+  num height = 5.9;
+  String country = "Egypt";
+
+
+  print("My name is $fName $lName. I am $age years old. My phone number is $phone. I am $height feet tall and I live in $country.");
+}
